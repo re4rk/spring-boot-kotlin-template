49aae5fd633812a73a9cdcda85c502406b3897c4
re4rk <37167652+re4rk@users.noreply.github.com>
2025-04-02 02:40:01 +0900

    Add OpenTelemetry API and SDK dependencies
    Included OpenTelemetry API and SDK to enhance tracing support alongside Micrometer. This change improves compatibility with OpenTelemetry-based observability tools.

--- support/logging/build.gradle.kts
+++ support/logging/build.gradle.kts
@@ -2,5 +2,7 @@ dependencies {
     api("io.micrometer:micrometer-tracing")
     implementation("io.micrometer:micrometer-tracing-bridge-otel")
     implementation("io.micrometer:micrometer-observation")
+    implementation("io.opentelemetry:opentelemetry-api")
+    implementation("io.opentelemetry:opentelemetry-sdk")
     implementation("io.sentry:sentry-logback:${property("sentryVersion")}")
 }
